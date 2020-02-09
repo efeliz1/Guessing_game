052c2be5bdcee4c611f8c7b7b66a6c9c56d78664
Edwin <efeliz@student.fitchburgstate.edu>
2020-02-09 17:02:16 -0500

    fix bug from second loop

--- Guessing_game.c
+++ Guessing_game.c
@@ -7,7 +7,7 @@ int main(void)
 	//low = lowest number input
 	// high = highest number input 
 	//middle = middle between low + high
-	int low, high, middle, search_space,counter;
+	int low, high, middle,counter,search_space;
 	char answer;
 
 
@@ -21,36 +21,42 @@ int main(void)
 
 	printf("Your low is %d and your high %d \n", low, high);
 
+	int num_low = low;
+	int num_high = high;
 
 
-	// int counter = 0;
-	
-	// while(low< high)
-	// {
-	// 	//cut search space by half
-	// 	middle = (high + low) / 2;
-	// 	low =middle + 1;
+	counter = 0;
+	 while(num_low< num_high)
+	{
+		//cut search space by half
+		search_space= (num_high + num_low) / 2;
+
+		num_low = search_space + 1;
 		
 
-	// 	//increment the counter
-	// 	counter = counter + 1; 
-	// }
-	// printf("\nI would guess your number in %d question\n", counter);
+	 	//increment the counter
+	 	counter = counter + 1; 
+	}
+	printf("\nI would guess your number in %d question\n", counter);
 	
 
-	// counter = 0;
+	counter = 0;
 
 	while (low <= high)
 	{
 		middle = (high + low) / 2;
 
+		printf("\nQuestion %d \n", counter);
+
 		printf("Please enter e if your number is equal to %d \n", middle);
 		printf("Please enter s if your number is smaller to %d \n", middle);
-		printf("Please enter g if your number is greater to %d \n", middle);
+		printf("Please enter g if your number is greater to %d \n",  middle);
 		scanf(" %c", &answer);
 
-		if (answer == 'e') // E = correct number
-		{
+		
+
+		if (answer == 'e')
+		{	// E = correct answer
 			printf("\nCorrect \n");
 		}
 
@@ -58,17 +64,14 @@ int main(void)
 		{
 			if (answer == 's') // S = smaller number
 			{
-				high = middle - 1;
+				high = search_space - 1;
 			}
 			if (answer == 'g')
 			{
-				low = middle + 1;
+				low = search_space + 1;
 			}
 		}
-		if(low == middle || low == high)
-		{
-			printf("You're cheating");
-		}
+
 		counter = counter + 1;
 	}
 
